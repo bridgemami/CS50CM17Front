f74b37eb47ea3b733dfe5d4f068fdc52fe32bf12
Michael <80859948+bridgemami@users.noreply.github.com>
2021-12-09 01:40:54 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+/* Students: Please use this week's project for Week 16: Assignment 14: Your Mobile App. 
+     You will need to replace the contents of this JavaScript file with your own work, 
+     and create any other files, if any, required for the assignment.
+     When you are done, be certain to submit the assignment in both Repl.it and Canvas to be graded. */
+'use strict';
+console.log("let's start");
+// define a react page component using traditional pure JS
+function FinalList(final) {
+  // return some output for the component using JSX
+  return <div className="list">
+  <h1>{final.message1}</h1>
+  <ol>
+  {
+    // use map() to loop thru array passed in props.list, creating new element for each array value
+    final.list.map(
+      (item, index) => (
+        <li key={index}>
+        <h3><a href={item.url} alt={item.alt}>{item.title}</a></h3>
+        <a href={item.url}><img src={item.imagepath} /></a><p>
+        {item.description}<br /> {item.date}</p>
+        <hr />
+        </li>
+      )
+    )
+  }
+  </ol>
+  </div>
+}
+console.log("props function");
+// load dataset from external JSON using jQuery
+jQuery.getJSON(
+  //getJson() takes 2 arguments: 1st is the path to file to JSON file and 2nd is a function to run with data
+  'https://cs50c-week17-server-side-23-michaelbridgema.srjcethanwilde.repl.co/assignments', function(jsonFromJquery) {
+    //inside this function code can run that accesses the json data
+    console.log(jsonFromJquery);
+  // call react render() to output a component into an existing html element
+    // pass properties as attributes: list passes array stored in var named data
+    // message1  passed as literal values
+    ReactDOM.render(
+      <FinalList list={jsonFromJquery} message1= "My Assignments from CS50C" />,
+      document.getElementById('myFinal')
+    );
+  }
+);
